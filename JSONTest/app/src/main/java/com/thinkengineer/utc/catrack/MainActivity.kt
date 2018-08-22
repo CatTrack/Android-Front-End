eb4e4b455df043009f3b7aff5ece402f76d30bbd
Ben Emmons <ben@emmons.biz>
2018-08-22 11:48:04 +0100

    Make functions more modular to make code cleaner.

--- JSONTest/app/src/main/java/com/thinkengineer/utc/catrack/MainActivity.kt
+++ JSONTest/app/src/main/java/com/thinkengineer/utc/catrack/MainActivity.kt
@@ -9,7 +9,7 @@ import kotlinx.serialization.Serializable
 import kotlinx.serialization.json.JSON
 
 @Serializable
-data class data(var userID: String="", var catName: String="", var photoURI: String="")
+data class Data(var userID: String="", var catName: String="", var photoURI: String="")
 
 class MainActivity : AppCompatActivity() {
 
@@ -18,22 +18,21 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         submitButton.setOnClickListener {
-            val catData = data()
-            catData.userID = userID_Field.text.toString()
-            catData.catName = catName_Field.text.toString()
-            catData.photoURI = photoURI_Field.text.toString()
-            System.out.println(JSON.stringify(catData))
-            FuelManager.instance.baseHeaders = mapOf("Content-Type" to "application/json")
-            Fuel.post("https://us-central1-te-cattrack.cloudfunctions.net/newCat").body(JSON.stringify(catData)).response { request, response, result ->
-                System.out.println(request)
-                System.out.println(response)
-                System.out.println(result)
-            }
-
-
+            newCat()
         }
     }
 
-
-
+    private fun newCat() {
+        val catData = Data()
+        catData.userID = userID_Field.text.toString()
+        catData.catName = catName_Field.text.toString()
+        catData.photoURI = photoURI_Field.text.toString()
+        System.out.println(JSON.stringify(catData))
+        FuelManager.instance.baseHeaders = mapOf("Content-Type" to "application/json")
+        Fuel.post("https://us-central1-te-cattrack.cloudfunctions.net/newCat").body(JSON.stringify(catData)).response { request, response, result ->
+            System.out.println(request)
+            System.out.println(response)
+            System.out.println(result)
+        }
+    }
 }
