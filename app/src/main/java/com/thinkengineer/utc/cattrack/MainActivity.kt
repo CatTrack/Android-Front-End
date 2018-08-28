63414e8afe849cc95bb975e89e87651b44ee2cc5
Benji <benji@benjicraft.info>
2018-08-28 16:09:59 +0100

    // WIP

--- app/src/main/java/com/thinkengineer/utc/cattrack/MainActivity.kt
+++ app/src/main/java/com/thinkengineer/utc/cattrack/MainActivity.kt
@@ -11,6 +11,7 @@ import com.google.firebase.database.DatabaseError
 import com.google.firebase.database.FirebaseDatabase
 import com.google.firebase.database.ValueEventListener
 import kotlinx.serialization.Serializable
+import kotlinx.serialization.json.JSON
 
 @Serializable
 data class Data(var userID: String="", var catName: String="", var photoURI: String="", var catID: String="")
@@ -23,6 +24,7 @@ class MainActivity : AppCompatActivity() {
     private lateinit var logout: Button
     private lateinit var auth: FirebaseAuth
     private lateinit var database: FirebaseDatabase
+    private lateinit var UID: String
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -47,36 +49,61 @@ class MainActivity : AppCompatActivity() {
         }
         isLogin()
 
-    }
+        UID = FirebaseAuth.getInstance().currentUser!!.uid
+        System.out.println(UID);
 
+        /* val catData = Data()
+        catData.userID = userID_Field.text.toString()
+        System.out.println(JSON.stringify(catData))
+        FuelManager.instance.baseHeaders = mapOf("Content-Type" to "application/json")
+        Fuel.get("https://us-central1-te-cattrack.cloudfunctions.net/getCats", listOf("userID" to catData.userID )).responseString { request, response, result ->
+            result.fold({ cats ->
+                System.out.println(cats)
+                /* Give object name so it matches Data class */
+                var catObject = JSON.parse<Cats>("{\"cats\": $cats}")
+                // Blame Andrejus
 
-
-    private fun isLogin(){
-        val intent = Intent(this@MainActivity, LoginActivity::class.java)
-
-        auth.currentUser?.uid?.let { loadData(it)  } ?: startActivity(intent)
-
-    }
-
-    private fun loadData(userId: String){
-        val dataListener = object : ValueEventListener{
-            override fun onCancelled(p0: DatabaseError) {
-                TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
-            }
-
-            override fun onDataChange(dataSnapshot: DataSnapshot) {
-                if(dataSnapshot.exists()){
-                    var user: User = dataSnapshot.getValue(User::class.java)!!
-                    displayName.text = user.displayName
-                    status.text = user.status
+                var catsFormatted = ""
+                catObject.cats.forEach { cat ->
+                    catsFormatted += "\n$cat"
                 }
-
-            }
-        }
-        database.reference.child("user")
-                .child(userId).addListenerForSingleValueEvent(dataListener)
-
+                responseOutput.text = catsFormatted
+            }, {
+                error -> responseOutput.text = error.toString()
+            })
+        }*/
     }
+
+}
+
+
+
+private fun isLogin(){
+    val intent = Intent(this@MainActivity, LoginActivity::class.java)
+
+    auth.currentUser?.uid?.let { loadData(it)  } ?: startActivity(intent)
+
+}
+
+private fun loadData(userId: String){
+    val dataListener = object : ValueEventListener{
+        override fun onCancelled(p0: DatabaseError) {
+            TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        }
+
+        override fun onDataChange(dataSnapshot: DataSnapshot) {
+            if(dataSnapshot.exists()){
+                var user: User = dataSnapshot.getValue(User::class.java)!!
+                displayName.text = user.displayName
+                status.text = user.status
+            }
+
+        }
+    }
+    database.reference.child("user")
+            .child(userId).addListenerForSingleValueEvent(dataListener)
+
+}
 
 
 
