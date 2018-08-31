f53415e64d8fd74f8280cc719cde9e6f83816277
Unknown <reilly.hewitson@utcreading.org.uk>
2018-08-31 10:17:58 +0100

    GET works

--- app/src/main/java/com/thinkengineer/utc/cattrack/CatActivity.kt
+++ app/src/main/java/com/thinkengineer/utc/cattrack/CatActivity.kt
@@ -9,12 +9,28 @@ import com.android.volley.Request
 import com.android.volley.Response
 import com.android.volley.toolbox.StringRequest
 import com.android.volley.toolbox.Volley
-
+import com.beust.klaxon.Parser
+import com.beust.klaxon.json
+import com.github.kittinunf.fuel.android.extension.jsonDeserializer
+import com.github.kittinunf.fuel.android.extension.responseJson
+import com.github.kittinunf.fuel.httpGet
+import com.google.gson.Gson
+import com.google.gson.GsonBuilder
+import com.google.gson.reflect.TypeToken
 import kotlinx.android.synthetic.main.activity_cat.*
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.FirebaseUser
+import com.google.gson.JsonObject
+import com.thinkengineer.utc.cattrack.CatActivity.c.getHTML
+import khttp.get
 import org.json.JSONObject
 import java.net.URL
+//import jdk.nashorn.internal.runtime.ScriptingFunctions.readLine
+import java.io.BufferedReader
+import java.io.InputStreamReader
+import java.lang.StringBuilder
+import java.net.HttpURLConnection
+
 
 val currentFirebaseUser = FirebaseAuth.getInstance().currentUser
 
@@ -23,21 +39,27 @@ class CatActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_cat)
-        var catIntent = getIntent()
-        var catID = catIntent.getIntExtra("catNum", 1)
-        val textView = findViewById<TextView>(R.id.name)
-        val currentFirebaseUser = FirebaseAuth.getInstance().currentUser
-// ...
+        kotlin.run {
+            Thread( Runnable{
+            var catIntent = getIntent()
+            var catID = catIntent.getStringExtra("catNum")
+                val textView = findViewById<TextView>(R.id.name)
+            val FUID = catIntent.getStringExtra("userID")
+            // ...
+            // Instantiate the RequestQueue.
+            val Url = "http://us-central1-te-cattrack.cloudfunctions.net/getCatsAdv?userID=0"//?userID=" + FUID
 
-// Instantiate the RequestQueue.
-        val url = "https://us-central1-te-cattrack.cloudfunctions.net/listCats?userID="+currentFirebaseUser
-
-// Request a string response from the provided URL.
-        val catList = URL(url).getContent()
-        val json: JSONObject = catList as JSONObject
-        val catObject: JSONObject = json.getJSONObject(catID.toString())
-        val name = catObject.getString("Identifier")
-        textView.text = name
+            // Request a string response from the provided URL.
+            val catList = getHTML(Url)
+                print(catList)
+                val parser = Parser()
+                val stringBuilder: StringBuilder = StringBuilder(catList)
+                val catListObject: JSONObject = parser.parse(stringBuilder) as JSONObject
+                val CatObject: JSONObject = catListObject.getJSONObject("0")
+            val Identity = CatObject.getJSONObject("Identifier")
+            val name = Identity.getString("stringValue")
+                runOnUiThread(Runnable { textView.text = name })
+        }).start()}
 
 // Add the request to the RequestQueue.
 
@@ -45,6 +67,26 @@ class CatActivity : AppCompatActivity() {
 
     override fun getIntent(): Intent {
         return super.getIntent()
+    }
+
+    object c {
+
+        @Throws(Exception::class)
+        fun getHTML(urlToRead: String): String {
+            val url = URL(urlToRead)
+            val conn = url.openConnection() as HttpURLConnection
+            conn.setRequestMethod("GET")
+            val rd = BufferedReader(InputStreamReader(conn.getInputStream()))
+            val result = rd.toString()
+
+            return result
+        }
+
+        @Throws(Exception::class)
+        @JvmStatic
+        fun main(args: Array<String>) {
+            println(getHTML(args[0]))
+        }
     }
 
 
--- app/src/main/java/com/thinkengineer/utc/cattrack/MainActivity.kt
+++ app/src/main/java/com/thinkengineer/utc/cattrack/MainActivity.kt
@@ -9,6 +9,7 @@ import com.thinkengineer.utc.cattrack.CatActivity;
 import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import android.widget.Button
 import android.widget.HorizontalScrollView
 import android.widget.LinearLayout
@@ -27,26 +28,26 @@ class MainActivity : AppCompatActivity() {
     private lateinit var logout: Button
     private lateinit var auth: FirebaseAuth
     private lateinit var database: FirebaseDatabase
+    private lateinit var user: String
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         auth = FirebaseAuth.getInstance()
         database = FirebaseDatabase.getInstance()
-
+        user = FirebaseAuth.getInstance().getCurrentUser().toString()
         displayName = findViewById<TextView>(R.id.nameTextView)
         status = findViewById<TextView>(R.id.statusTextView)
         logout = findViewById<Button>(R.id.singoutButton)
         cat1Button = findViewById<Button>(R.id.cat1button)
-
+        print(user)
+        print("test")
         cat1Button.setOnClickListener({
-            @Override
-            fun onClick() {
-                val intent =  Intent(applicationContext, CatActivity::onCreate.javaClass)
+                val intent =  Intent(applicationContext, CatActivity::class.java)
                         intent.putExtra("catNum", "0")
+                        intent.putExtra("userID", user)
                 startActivity(intent)
-            }
-        })
+            })
 
         logout.setOnClickListener(){
             auth.signOut()
@@ -70,8 +71,7 @@ class MainActivity : AppCompatActivity() {
     private fun loadData(userId: String){
         val dataListener = object : ValueEventListener{
             override fun onCancelled(p0: DatabaseError) {
-                TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
-            }
+                }
 
             override fun onDataChange(dataSnapshot: DataSnapshot) {
                 if(dataSnapshot.exists()){
